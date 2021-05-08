3f0f4341548fc1283b087c20e46d8a709317ae8b
yuelinn <yuelinnchong@gmail.com>
2021-05-08 23:19:05 +0800

    moved callback to its own script

---
+++ flightrl/examples/rand_goals_callback.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+'''
+@script description
+
+'''
+
+# builtin 
+
+# third-party
+import torch as th
+
+# own mods
+
+__author__ = "Linn of the Ded"
+__status__ = "Production"
+
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+class RandGoalsCallback(BaseCallback):
+    """
+    A custom callback that derives from ``BaseCallback``.
+    :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
+    """
+    def __init__(self, waypt_gap_m=10.0, goal_tol_m=3.0, verbose=0):
+    # def __init__(self, callback: Optional[BaseCallback] = None, verbose: int = 0):
+        super(RandGoalsCallback, self).__init__(verbose)
+        # Those variables will be accessible in the callback
+        # (they are defined in the base class)
+        # The RL model
+        # self.model = None  # type: BaseAlgorithm
+        # An alias for self.model.get_env(), the environment used for training
+        # self.training_env = None  # type: Union[gym.Env, VecEnv, None]
+        # Number of time the callback was called
+        # self.n_calls = 0  # type: int
+        # self.num_timesteps = 0  # type: int
+        # local and global variables
+        # self.locals = None  # type: Dict[str, Any]
+        # self.globals = None  # type: Dict[str, Any]
+        # The logger object, used to report things in the terminal
+        # self.logger = None  # stable_baselines3.common.logger
+        # # Sometimes, for event callback, it is useful
+        # # to have access to the parent object
+        # self.parent = None  # type: Optional[BaseCallback]
+        print("Setting rand goals for training...")
+        self.waypt_gap_m=waypt_gap_m # max distance between the gen goal n the robot at time of gen
+        self.goal_tol_m=goal_tol_m # min dist to consider the goal is met
+        self.goal=np.array([5.0, 5.0, 5.0], dtype=np.float32)
+        self.is_reached_goal=False # TODO: this is not used but is set correctly
+        self.rollout_id=0
+
+    def _on_training_start(self) -> None:
+        """
+        This method is called before the first rollout starts.
+        """
+        pass
+
+    def _on_rollout_start(self) -> None:
+        """
+        A rollout is the collection of environment interaction
+        using the current policy.
+        This event is triggered before collecting new samples.
+        """
+
+        self.rollout_id +=1
+        if self.rollout_id % 300 ==0 :
+            print("300 rollouts, new goal")
+
+            offset=np.random.uniform(self.waypt_gap_m*-1, self.waypt_gap_m, size=(3))
+
+            # if it is too close to curr pose
+            while np.absolute(offset[0]) < 1.0 or np.absolute(offset[1]) < 1.0:
+                # TODO: I could make this more efficient but I dun feel like it...
+                offset=np.random.uniform(self.waypt_gap_m*-1, self.waypt_gap_m, size=(3))
+
+            # TODO: make goal offset fr curr pos. rmb to check bounds
+            new_goal=offset
+
+            # check bounds
+            new_goal[2]=np.random.uniform(0.1, 8.0)
+
+            print("new goal ", new_goal)
+    
+            self.goal=np.array(new_goal, dtype=np.float32)
+            self.training_env.set_goal(self.goal)
+            self.is_reached_goal=False
+
+    def _on_step(self) -> bool:
+        """
+        This method will be called by the model after each call to `env.step()`.
+        For child callback (of an `EventCallback`), this will be called
+        when the event is triggered.
+        :return: (bool) If the callback returns False, training is aborted early.
+        """
+        
+        # print("global keys", self.globals.keys())
+        # print("local keys", self.locals.keys())
+
+        
+        obs_tensor=(self.locals["new_obs"])
+        # obs_tensor=(self.locals["obs_tensor"]).numpy()
+        print("obs: ", self.locals["new_obs"])
+
+        # check if near goal
+        pos=obs_tensor[0,:3]
+        dist=np.sqrt(np.sum((self.goal-pos) ** 2))
+
+        if dist < self.goal_tol_m: # if reached goal
+            self.is_reached_goal=True 
+
+        # diff and normalise before giving PPO
+        new_obs=copy.deepcopy(obs_tensor)
+        new_obs[0,0]=obs_tensor[0,0]-obs_tensor[0,12]
+        new_obs[0,1]=obs_tensor[0,1]-obs_tensor[0,13]
+        new_obs[0,2]=obs_tensor[0,2]-obs_tensor[0,14]
+        new_obs[0,12]=0.0
+        new_obs[0,13]=0.0
+        new_obs[0,14]=0.0
+        new_obs=new_obs/10.0
+        self.locals["new_obs"]=new_obs
+        self.locals["obs_tensor"]=torch.from_numpy(new_obs)
+
+        return True
+
+    def _on_rollout_end(self) -> None:
+        """
+        This event is triggered before updating the policy.
+        """
+        pass
+
+    def _on_training_end(self) -> None:
+        """
+        This event is triggered before exiting the `learn()` method.
+        """
+        pass
+
--- flightrl/examples/run_drone_control.py
+++ flightrl/examples/run_drone_control.py
@@ -18,7 +18,6 @@ from stable_baselines3.common import logger
 #
 from stable_baselines3.ppo.ppo import PPO
 from stable_baselines3.sac.sac import SAC
-from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.callbacks import CheckpointCallback
 
 from rpg_baselines.ppo.ppo2_test import test_model
@@ -28,131 +27,7 @@ from scipy.spatial.transform import Rotation
 #
 from flightgym import QuadrotorEnv_v1
 
-
-class RandGoalsCallback(BaseCallback):
-    """
-    A custom callback that derives from ``BaseCallback``.
-
-    :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
-    """
-    def __init__(self, waypt_gap_m=10.0, goal_tol_m=3.0, verbose=0):
-    # def __init__(self, callback: Optional[BaseCallback] = None, verbose: int = 0):
-        super(RandGoalsCallback, self).__init__(verbose)
-        # Those variables will be accessible in the callback
-        # (they are defined in the base class)
-        # The RL model
-        # self.model = None  # type: BaseAlgorithm
-        # An alias for self.model.get_env(), the environment used for training
-        # self.training_env = None  # type: Union[gym.Env, VecEnv, None]
-        # Number of time the callback was called
-        # self.n_calls = 0  # type: int
-        # self.num_timesteps = 0  # type: int
-        # local and global variables
-        # self.locals = None  # type: Dict[str, Any]
-        # self.globals = None  # type: Dict[str, Any]
-        # The logger object, used to report things in the terminal
-        # self.logger = None  # stable_baselines3.common.logger
-        # # Sometimes, for event callback, it is useful
-        # # to have access to the parent object
-        # self.parent = None  # type: Optional[BaseCallback]
-        print("Setting rand goals for training...")
-        self.waypt_gap_m=waypt_gap_m # max distance between the gen goal n the robot at time of gen
-        self.goal_tol_m=goal_tol_m # min dist to consider the goal is met
-        self.goal=np.array([5.0, 5.0, 5.0], dtype=np.float32)
-        self.is_reached_goal=False
-        self.rollout_id=0
-
-    def _on_training_start(self) -> None:
-        """
-        This method is called before the first rollout starts.
-        """
-        pass
-
-    def _on_rollout_start(self) -> None:
-        """
-        A rollout is the collection of environment interaction
-        using the current policy.
-        This event is triggered before collecting new samples.
-        """
-
-        # if(self.is_reached_goal):
-        self.rollout_id +=1
-        if self.rollout_id % 300 ==0 :
-            print("300 rollouts, new goal")
-
-            offset=np.random.uniform(self.waypt_gap_m*-1, self.waypt_gap_m, size=(3))
-
-            # if it is too close to curr pose
-            while np.absolute(offset[0]) < 1.0 or np.absolute(offset[1]) < 1.0:
-                # TODO: I could make this more efficient but I dun feel like it...
-                offset=np.random.uniform(self.waypt_gap_m*-1, self.waypt_gap_m, size=(3))
-
-            # TODO: make goal offset fr curr pos. rmb to check bounds
-            new_goal=offset
-
-            # check bounds
-            new_goal[2]=np.random.uniform(0.1, 8.0)
-
-            print("new goal ", new_goal)
-
-            self.goal=np.array(new_goal, dtype=np.float32)
-
-            self.training_env.set_goal(self.goal)
-
-
-    def _on_step(self) -> bool:
-        """
-        This method will be called by the model after each call to `env.step()`.
-
-        For child callback (of an `EventCallback`), this will be called
-        when the event is triggered.
-
-        :return: (bool) If the callback returns False, training is aborted early.
-        """
-        
-        # print("global keys", self.globals.keys())
-        # print("local keys", self.locals.keys())
-
-        # check if near goal
-        obs_tensor=(self.locals["new_obs"])
-        print("obs: ", self.locals["new_obs"])
-
-        # obs_tensor=(self.locals["obs_tensor"]).numpy()
-        pos=obs_tensor[0,:3]
-        # goal=obs_tensor[0,-3:]
-        dist=np.sqrt(np.sum((self.goal-pos) ** 2))
-
-        if dist < self.goal_tol_m: # if reached goal
-            self.is_reached_goal=True # set a new goal
-
-        # diff and normalise before giving PPO
-        new_obs=copy.deepcopy(obs_tensor)
-        new_obs[0,0]=obs_tensor[0,0]-obs_tensor[0,12]
-        new_obs[0,1]=obs_tensor[0,1]-obs_tensor[0,13]
-        new_obs[0,2]=obs_tensor[0,2]-obs_tensor[0,14]
-        new_obs[0,12]=0.0
-        new_obs[0,13]=0.0
-        new_obs[0,14]=0.0
-        new_obs=new_obs/10.0
-        self.locals["new_obs"]=new_obs
-        self.locals["obs_tensor"]=torch.from_numpy(new_obs)
-
-        return True
-
-    def _on_rollout_end(self) -> None:
-        """
-        This event is triggered before updating the policy.
-        """
-        pass
-
-    def _on_training_end(self) -> None:
-        """
-        This event is triggered before exiting the `learn()` method.
-        """
-        pass
-
-
-
+from rand_goals_callback import RandGoalsCallback
 
 def configure_random_seed(seed, env=None):
     if env is not None:
@@ -223,8 +98,12 @@ def main():
         log_dir = rsg_root + '/saved'
         saver = U.ConfigurationSaver(log_dir=log_dir)
 
-        model = SAC('MlpPolicy', env, verbose=2, tensorboard_log=saver.data_dir)
+        # SAC version
+        # model = SAC('MlpPolicy', env, verbose=2, tensorboard_log=saver.data_dir)
+        model=SAC.load("./saved/2021-05-08-12-12-16/weights/w_time__180000_steps.zip") # fine tuning from hover policy
+        model.set_env(env)
 
+        # PPO version
         # model = PPO('MlpPolicy', env, verbose=2, tensorboard_log=saver.data_dir)
         # model=PPO.load("./saved/2021-05-03-20-34-51.zip") # fine tuning from hover policy
         # model.set_env(env)
